c1476db7b26ef2b9cf1f6f04279a966d7df86be2
pang_dodo <pang_dodo@163.com>
2015-04-13 19:49:45 +0800

    数据库连接

--- pams/src/ioc.js
+++ pams/src/ioc.js
@@ -12,9 +12,9 @@ var ioc={
         fields : 
         {
            driverClassName:'oracle.jdbc.driver.OracleDriver',
-           url:'jdbc:oracle:thin:@192.168.1.102:1521:orcl',
+           url:'jdbc:oracle:thin:@192.168.135.132:1521:orcl',
            username:'pams',
-           password:'pams123'
+           password:'pams1234'
         }
     },
     

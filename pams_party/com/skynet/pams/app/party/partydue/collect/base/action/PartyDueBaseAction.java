5f9fba9eb655fc008918cf4a6d707516e7499ed1
skynetbird <skynetbird@126.com>
2015-04-15 15:34:45 +0800

    更新

--- pams_party/com/skynet/pams/app/party/partydue/collect/base/action/PartyDueBaseAction.java
+++ pams_party/com/skynet/pams/app/party/partydue/collect/base/action/PartyDueBaseAction.java
@@ -3,7 +3,6 @@ package com.skynet.pams.app.party.partydue.collect.base.action;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +10,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-import org.nutz.dao.Cnd;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.mvc.Mvcs;
@@ -25,10 +23,10 @@ import com.skynet.app.workflow.vo.VApply;
 import com.skynet.framework.action.BaseAction;
 import com.skynet.framework.common.generator.UUIDGenerator;
 import com.skynet.framework.services.db.dybeans.DynamicObject;
-import com.skynet.pams.app.party.service.PartyDueUseSuggestDetailService;
-import com.skynet.pams.app.party.service.PartyDueUseSuggestService;
+import com.skynet.pams.app.party.service.PartyDueBaseDetailService;
+import com.skynet.pams.app.party.service.PartyDueBaseService;
 import com.skynet.pams.app.plan.service.PlanService;
-import com.skynet.pams.base.pojo.PartyDueUseSuggest;
+import com.skynet.pams.base.pojo.PartyDueBase;
 import com.skynet.pams.base.pojo.Plan;
 
 @IocBean
@@ -36,18 +34,18 @@ import com.skynet.pams.base.pojo.Plan;
 public class PartyDueBaseAction extends BaseAction {
 
 	@Inject
-	private PartyDueUseSuggestService partydueusesuggestService;
+	private PartyDueBaseService partyduebaseService;
 	
 	@Inject
-	private PartyDueUseSuggestDetailService partydueusesuggestdetailService;
+	private PartyDueBaseDetailService partyduebasedetailService;
 	
 	@Inject
 	private PlanService planService;
 	
-	public static String tableid = "PartyDueUseSuggest";
+	public static String tableid = "PartyDueBase";
 	
 	@At("/mainframe")
-	@Ok("->:/page/party/partydue/research/usesuggest/mainframe.ftl")
+	@Ok("->:/page/party/partydue/collect/base/mainframe.ftl")
 	public Map mainframe() throws Exception
 	{
 		return ro;
@@ -66,13 +64,12 @@ public class PartyDueBaseAction extends BaseAction {
 		Plan plan = planService.get(planid);
 		
 		Timestamp nowtime = new Timestamp(System.currentTimeMillis());
-		PartyDueUseSuggest usesuggest = new PartyDueUseSuggest();
-		usesuggest.setId(UUIDGenerator.getInstance().getNextValue());
-		usesuggest.setCname(plan.getCname());
-		usesuggest.setCreatetime(nowtime);
-		usesuggest.setCreater(loginname);
-		usesuggest.setCreatercname(username);
-		usesuggest.setCyear((new GregorianCalendar()).get(Calendar.YEAR));
+		PartyDueBase base = new PartyDueBase();
+		base.setId(UUIDGenerator.getInstance().getNextValue());
+		base.setCreatetime(nowtime);
+		base.setCreater(loginname);
+		base.setCreatercname(username);
+		base.setCyear((new GregorianCalendar()).get(Calendar.YEAR));
 		
 		DynamicObject swapFlow = new DynamicObject();
 		swapFlow.setAttr(GlobalConstants.swap_coperatorid, userid);
@@ -81,14 +78,14 @@ public class PartyDueBaseAction extends BaseAction {
 		swapFlow.setAttr(GlobalConstants.swap_flowdefid, flowdefid);
 		swapFlow.setAttr(GlobalConstants.swap_tableid, tableid);
 
-		String runactkey = partydueusesuggestService.plancreate(plan, usesuggest, swapFlow);
+		String runactkey = partyduebaseService.plancreate(plan, base, swapFlow);
 		
 		ro.put("runactkey", runactkey);
 		return ro;
 	}
 	
 	@At("/browseplan")
-	@Ok("->:/page/party/partydue/research/usesuggest/browseplan.ftl")
+	@Ok("->:/page/party/partydue/collect/base/browseplan.ftl")
 	public Map browseplan() {
 		return ro;
 	}
@@ -108,14 +105,14 @@ public class PartyDueBaseAction extends BaseAction {
 		amap.put(GlobalConstants.swap_coperatorid, userid);
 		amap.put(GlobalConstants.swap_coperatorloginname, loginname);
 
-		List datas = partydueusesuggestService.sdao().queryForList(
-				partydueusesuggestService.get_browseplan_sql(amap));
+		List datas = partyduebaseService.sdao().queryForList(
+				partyduebaseService.get_browseplan_sql(amap));
 
 		return datas; // 后期改为翻页对象
 	}
 
 	@At("/browsewait")
-	@Ok("->:/page/party/partydue/research/usesuggest/browsewait.ftl")
+	@Ok("->:/page/party/partydue/collect/base/browsewait.ftl")
 	public Map browsewait() {
 		return ro;
 	}
@@ -138,14 +135,14 @@ public class PartyDueBaseAction extends BaseAction {
 
 		amap.put("flowcclass", flowcclass);
 
-		List datas = partydueusesuggestService.sdao().queryForList(
-				partydueusesuggestService.get_browsewait_sql(amap));
+		List datas = partyduebaseService.sdao().queryForList(
+				partyduebaseService.get_browsewait_sql(amap));
 
 		return datas; // 后期改为翻页对象
 	}
 	
 	@At("/browsehandle")
-	@Ok("->:/page/party/partydue/research/usesuggest/browsehandle.ftl")
+	@Ok("->:/page/party/partydue/collect/base/browsehandle.ftl")
 	public Map browsehandle() {
 		return ro;
 	}
@@ -168,14 +165,14 @@ public class PartyDueBaseAction extends BaseAction {
 
 		amap.put("flowcclass", flowcclass);
 
-		List datas = partydueusesuggestService.sdao().queryForList(
-				partydueusesuggestService.get_browsehandle_sql(amap));
+		List datas = partyduebaseService.sdao().queryForList(
+				partyduebaseService.get_browsehandle_sql(amap));
 
 		return datas; // 后期改为翻页对象
 	}
 	
 	@At("/browseall")
-	@Ok("->:/page/party/partydue/research/usesuggest/browseall.ftl")
+	@Ok("->:/page/party/partydue/collect/base/browseall.ftl")
 	public Map browseall() {
 		return ro;
 	}
@@ -198,21 +195,21 @@ public class PartyDueBaseAction extends BaseAction {
 
 		amap.put("flowcclass", flowcclass);
 
-		List datas = partydueusesuggestService.sdao().queryForList(
-				partydueusesuggestService.get_browseall_sql(amap));
+		List datas = partyduebaseService.sdao().queryForList(
+				partyduebaseService.get_browseall_sql(amap));
 
 		return datas; // 后期改为翻页对象
 	}
 	
 	@At("/readpage")
-	@Ok("->:/page/party/partydue/research/usesuggest/readpage.ftl")
+	@Ok("->:/page/party/partydue/collect/base/readpage.ftl")
 	public Map readpage(String runactkey) throws Exception {
 		loc(runactkey);
 		return ro;
 	}
 
 	@At("/locate")
-	@Ok("->:/page/party/partydue/research/usesuggest/locate.ftl")
+	@Ok("->:/page/party/partydue/collect/base/locate.ftl")
 	public Map locate(String runactkey) throws Exception {
 		loc(runactkey);
 		return ro;
@@ -229,35 +226,57 @@ public class PartyDueBaseAction extends BaseAction {
 		
 		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
 		
-		DynamicObject ract = partydueusesuggestService.getWorkFlowEngine().getActManager()
+		DynamicObject ract = partyduebaseService.getWorkFlowEngine().getActManager()
 				.getRactService().locate(runactkey);
 		String id = ract.getFormatAttr("dataid");
 		String tableid = ract.getFormatAttr("tableid");
-		DynamicObject usesuggest = partydueusesuggestService.locate(id);
-		DynamicObject usesuggestdetail = partydueusesuggestdetailService.locateBy(Cnd.where("suggestid", "=", id).and("deptid", "=", deptid));	
+		DynamicObject base = partyduebaseService.locate(id);
+
 		
-		usesuggestdetail.setAttr("deptid", deptid);
-		usesuggestdetail.setAttr("deptname", deptname);
-		usesuggestdetail.setAttr("suggester", loginname);
-		usesuggestdetail.setAttr("suggestercname", username);
-		usesuggestdetail.setAttr("suggesttime", sf.format(new Date()));
+		List<DynamicObject> deptbasedetails = partyduebaseService.browsedeptbasedetails(id, deptid); // 本部门基数核准
 		
-		
-		List<DynamicObject> usesuggestdetails = partydueusesuggestdetailService.findByCond(Cnd.where("suggestid", "=", id)); // 汇总意见
+		List<DynamicObject> basedetails = partyduebaseService.browseallbasedetails(id); // 基数核准汇总
 		// 权限设置
 		set_author();
 		
 		// 查询可选路由
 		String actdefid = ract.getFormatAttr("actdefid");
-		List<DynamicObject> routes = partydueusesuggestService.getWorkFlowEngine().getFlowManager().getBrouteService().getRoutes(actdefid);
+		List<DynamicObject> routes = partyduebaseService.getWorkFlowEngine().getFlowManager().getBrouteService().getRoutes(actdefid);
 
 		ro.put("tableid", tableid);
 		ro.put("runactkey", runactkey);
-		ro.put("usesuggest", usesuggest);
-		ro.put("usesuggestdetail", usesuggestdetail);
-		ro.put("usesuggestdetails", usesuggestdetails);	
+		ro.put("base", base);
+		ro.put("deptbasedetails", deptbasedetails);
+		ro.put("basedetails", basedetails);	
 		ro.put("ract", ract);
 		ro.put("routes", routes);
+	}
+	
+	
+	
+	@At("/savedeptbasedetails")
+	@Ok("->:/page/party/partydue/collect/base/locate.ftl")
+	public Map savedeptbasedetails(String baseid, String[] baseuser, float[] base1, float[] base2, float[] base3, float[] base4) throws Exception {
+
+		HttpSession session = Mvcs.getHttpSession(true);
+		DynamicObject token = (DynamicObject)session.getAttribute(com.skynet.framework.spec.GlobalConstants.sys_login_token);
+		String loginname = token.getFormatAttr(com.skynet.framework.spec.GlobalConstants.sys_login_user);
+		String username = token.getFormatAttr(com.skynet.framework.spec.GlobalConstants.sys_login_username);		
+		String deptid = token.getFormatAttr(com.skynet.framework.spec.GlobalConstants.sys_login_dept);
+		String deptname = token.getFormatAttr(com.skynet.framework.spec.GlobalConstants.sys_login_deptname);
+		
+		DynamicObject map = new DynamicObject();
+		map.setAttr("baseid", baseid);
+		map.setAttr("deptid", deptid);
+		map.setObj("baseusers", baseuser);
+		map.setObj("base1s", base1);
+		map.setObj("base2s", base2);
+		map.setObj("base3s", base3);
+		map.setObj("base4s", base4);
+		
+		partyduebasedetailService.savedeptbasedetails(map);
+		
+		return ro;
 	}
 	
 	// 签收
@@ -268,7 +287,7 @@ public class PartyDueBaseAction extends BaseAction {
 		DynamicObject swapFlow = get_author_common();
 		
 		String loginname = swapFlow.getFormatAttr(GlobalConstants.swap_coperatorloginname);
-		boolean sign = partydueusesuggestService.getWorkFlowEngine().getActManager().enableApplyNew(runactkey, loginname, DBFieldConstants.PUB_PARTICIPATOR_PERSON);
+		boolean sign = partyduebaseService.getWorkFlowEngine().getActManager().enableApplyNew(runactkey, loginname, DBFieldConstants.PUB_PARTICIPATOR_PERSON);
 
 		if (!sign)
 		{
@@ -282,7 +301,7 @@ public class PartyDueBaseAction extends BaseAction {
 		vo.usertype = DBFieldConstants.PUB_PARTICIPATOR_PERSON;
 		vo.runactkey = runactkey;
 
-		partydueusesuggestService.getWorkFlowEngine().getActManager().vapply(vo);
+		partyduebaseService.getWorkFlowEngine().getActManager().vapply(vo);
 
 		ro.put("runactkey", runactkey);
 
@@ -296,16 +315,16 @@ public class PartyDueBaseAction extends BaseAction {
 		DynamicObject flowobj = get_author_common();
 
 		// 以下为常用权限；
-		boolean isread = partydueusesuggestService.isread(flowobj); // 是否只读页面
-		boolean isedit = partydueusesuggestService.isedit(flowobj); // 是否修改页面
+		boolean isread = partyduebaseService.isread(flowobj); // 是否只读页面
+		boolean isedit = partyduebaseService.isedit(flowobj); // 是否修改页面
 
-		boolean isapply = partydueusesuggestService.isapply(flowobj); // 可否签收
-		boolean isforward = partydueusesuggestService.isforward(flowobj); // 可否转发
-		boolean iscallback = partydueusesuggestService.iscallback(flowobj); // 可否收回
-		boolean isbackward = partydueusesuggestService.isbackward(flowobj); // 可否退回
+		boolean isapply = partyduebaseService.isapply(flowobj); // 可否签收
+		boolean isforward = partyduebaseService.isforward(flowobj); // 可否转发
+		boolean iscallback = partyduebaseService.iscallback(flowobj); // 可否收回
+		boolean isbackward = partyduebaseService.isbackward(flowobj); // 可否退回
 
-		boolean issave = partydueusesuggestService.issave(flowobj); // 可否保存
-		boolean isdelete = partydueusesuggestService.isdelete(flowobj); // 可否删除
+		boolean issave = partyduebaseService.issave(flowobj); // 可否保存
+		boolean isdelete = partyduebaseService.isdelete(flowobj); // 可否删除
 
 		// 以下为特定业务权限
 		
@@ -329,7 +348,7 @@ public class PartyDueBaseAction extends BaseAction {
 		DynamicObject swapFlow = ActionHelper.prepared_author_base();
 
 		String runactkey = Mvcs.getReq().getParameter("runactkey");
-		DynamicObject ract = partydueusesuggestService.getWorkFlowEngine().getActManager().getRactService().locate(runactkey);
+		DynamicObject ract = partyduebaseService.getWorkFlowEngine().getActManager().getRactService().locate(runactkey);
 		String runflowkey = ract.getFormatAttr("runflowkey");
 		String dataid = ract.getFormatAttr("dataid");
 		
